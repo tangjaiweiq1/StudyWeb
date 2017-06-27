62307eaedd07e3678e09122f57b180f45625f531
tangjaiweiq1 <644574747@qq.com>
2017-06-27 22:47:23 +0800

    Test

--- SpringTest/src/org/fkit/controller/HelloController.java
+++ SpringTest/src/org/fkit/controller/HelloController.java
@@ -2,26 +2,36 @@ package org.fkit.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//import org.springframework.web.servlet.mvc.Controller;
+///**
+// * HelloController 是一个实现Controller接口的控制器，
+// * 可以处理一个单一请求。
+// */
+//public class HelloController implements Controller {
 
 /**
- * HelloController 是一个实现Controller接口的控制器，
- * 可以处理一个单一请求。
+ * 可以同时处理多个请求，并且无需实现任何接口
  */
-public class HelloController implements Controller {
+@Controller
+public class HelloController {
     private static final Log logger = LogFactory.getLog(HelloController.class);
 
-    /**
-     * handleRequest是Controller接口必须的实现方法
-     * 对应参数是HttpServletRequest 和 HttpServletResponse
-     * 需要返回包含视图名的ModelAndView对象
-     */
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response) throws Exception {
+//    /**
+//     * handleRequest是Controller接口必须的实现方法
+//     * 对应参数是HttpServletRequest 和 HttpServletResponse
+//     * 需要返回包含视图名的ModelAndView对象
+//     */
+//    @Override
+//    public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response) throws Exception {
+
+@RequestMapping(value="/hello")
+    public ModelAndView hello() {
         logger.info("handleRequest 被调用");
         // 创建View对象，进行返回
         ModelAndView mv = new ModelAndView();
